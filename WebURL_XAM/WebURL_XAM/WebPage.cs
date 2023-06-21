87bc423bfedd19835c7a2c696e30b87dbcde7938
samalakarthik <skarthik@stratapps.com>
2023-06-21 11:45:46 +0530

    first commit

---
+++ WebURL_XAM/WebURL_XAM/WebPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WebURL_XAM
+{
+    public class WebPage: ContentPage
+    {
+       public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "https://xamplify.com/";
+            Content = browser;
+        }
+    }
+}
